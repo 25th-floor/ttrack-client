8d11f8db371a4ed58b35832218b6dce3f993dff7
Thomas Subera <ts@25th-floor.com>
2016-05-02 14:29:30 +0200

    allow console.error and ignore children

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
         'react/jsx-no-bind': [2, {
             'ignoreRefs': true,
         }],
+        'react/prop-types': [2, { ignore: ['children'] }],
         'no-use-before-define': 0,
+        'no-console': [1, { allow: ["error"] }],
     }
 };
