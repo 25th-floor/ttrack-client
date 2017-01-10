0e5db05d339fa6dcd755b60b3fdf655d0350f286
Thomas Subera <ts@25th-floor.com>
2017-01-10 17:19:41 +0100

    update eslint config to our latest standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,37 @@
 module.exports = {
-    'extends': '25th',
+    "extends": [
+        "airbnb",
+        "plugin:flowtype/recommended"
+    ],
     'parser': 'babel-eslint',
     'env': {
         'jest': true
     },
     'rules': {
+        'no-use-before-define': 0,
+        'no-console': [1, { allow: ["error", "info"] }],
+        "no-plusplus": [2, { "allowForLoopAfterthoughts": true }],
+        "indent": [2, 4, { "SwitchCase": 1 }],
+        "class-methods-use-this": 0,
         'react/jsx-no-bind': [2, {
             'ignoreRefs': true,
             'allowBind': true,
         }],
         'react/prop-types': [2, { ignore: ['children'] }],
-        'no-use-before-define': 0,
-        'no-console': [1, { allow: ["error", "info"] }],
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4],
+        "react/forbid-prop-types": 0,
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": 0,
+        "react/require-extension": 0,
+        "import/no-extraneous-dependencies": 0,
+        "max-len": ["error", 125, 4,
+            {
+                "ignoreUrls": true,
+                "ignoreComments": false,
+                "ignoreStrings": true,
+                "ignoreTemplateLiterals": true
+            }
+        ]
     }
 };
