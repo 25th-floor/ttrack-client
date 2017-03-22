40296dee4f9c1e44e27cb5a89c4a77cc02c6af69
Thomas Subera <ts@25th-floor.com>
2017-03-22 13:51:59 +0100

    also update user_calculate_carry_time function
    TTRACK-135

--- migrations/sqls/20170321150942-story-135-user-target-times-up.sql
+++ migrations/sqls/20170321150942-story-135-user-target-times-up.sql
@@ -131,6 +131,38 @@ $$;
 COMMENT ON FUNCTION user_worktime (INTEGER, DATE) IS 'calculate the overall working hours for a user until a given date.';
 
 
+-- calculates carry time for given user
+CREATE OR REPLACE FUNCTION user_calculate_carry_time(id INTEGER, due_date DATE)
+  RETURNS TABLE(
+    uw_date_from  DATE,
+    uw_due_date   DATE,
+    uw_carry_time INTERVAL
+  ) LANGUAGE plpgsql STABLE
+AS $$
+DECLARE
+  from_date  DATE;
+BEGIN
+  -- start with either start_timetracking, employment start or fallback
+  SELECT
+    LEAST(due_date, user_get_start_date(id))
+  INTO STRICT from_date
+  FROM users
+  WHERE usr_id = id;
+
+  RETURN QUERY SELECT
+                 from_date,
+                 due_date,
+                 SUM(sum_total_day)
+               FROM (
+                      SELECT
+                        (SUM(COALESCE(per_duration, '00:00:00') - COALESCE(per_break, '00:00:00')) - SUM(DISTINCT user_get_target_time(id, s :: DATE))) sum_total_day
+                      FROM generate_series(from_date, GREATEST(due_date, from_date), '1 day' :: INTERVAL) s
+                        LEFT JOIN days ON (day_date = s AND day_usr_id = id)
+                        LEFT JOIN periods ON (day_id = per_day_id)
+                      GROUP BY s :: DATE
+                    ) foo;
+END;
+$$;
 
 /* introduce new function to get start date for user since it's used multiple times */
 CREATE OR REPLACE FUNCTION user_add_new_target_time (id INTEGER, startdate DATE, target INTERVAL) RETURNS VOID
