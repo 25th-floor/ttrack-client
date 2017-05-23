f68f71629b0e45149569cb1bcce461e6cd119c10
Thomas Subera <ts@25th-floor.com>
2017-05-23 09:42:11 +0200

    add unit tests which fail for the bug

--- src/common/__tests__/timeUtils/timeUtils-getValidMoment-test.js
+++ src/common/__tests__/timeUtils/timeUtils-getValidMoment-test.js
@@ -60,7 +60,7 @@ describe('timeUtils.getValidMoment', function() {
                 expect(timeUtils.getValidMoment('-33', true).toJSON()).toEqual(moment.duration(-33, 'hours').toJSON());
             });
             it('if the value is -6:30 it returns valid moment', function() {
-                expect(timeUtils.getValidMoment('-6:30', true, true).toJSON()).toEqual(moment.duration(-390, 'minutes').toJSON());
+                expect(timeUtils.getValidMoment('-6:30', true).toJSON()).toEqual(moment.duration(-390, 'minutes').toJSON());
             });
             it('if the value is -7:30 it returns valid moment', function() {
                 expect(timeUtils.getValidMoment('-7:30', true).toJSON()).toEqual(moment.duration(-450, 'minutes').toJSON());
