3dba8a3af126b6272bc44451aec38216fae1a6f8
Philipp Bisson <pb@25th-floor.com>
2017-10-12 13:36:51 +0200

    wip

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -25,31 +25,33 @@ export class HomeContainer extends Component {
         return (
             <div className={styles['site-container']}>
                 <div className="container-fluid">
-                    <Navigation user={user} />
-                    <div className={styles.pageHeader}>
-                        <h1 className="hidden-lg hidden-md hidden-sm hidden-xs">Monats Ansicht</h1>
-                        <DatePicker
-                            activeMonth={Utils.getMomentToday()}
-                            years={Utils.getYearsForUser(user, Utils.getMomentToday())}
-                            months={Utils.getMonthsForUser(user, Utils.getMomentToday())}
-                            onChangeDate={this.handleChangeDate}
-                        />
-                        <div className="clearfix" />
+                    <div id={styles.month}>
+                        <Navigation user={user} />
+                        <div className={styles.pageHeader}>
+                            <h1 className="hidden-lg hidden-md hidden-sm hidden-xs">Monats Ansicht</h1>
+                            <DatePicker
+                                activeMonth={Utils.getMomentToday()}
+                                years={Utils.getYearsForUser(user, Utils.getMomentToday())}
+                                months={Utils.getMonthsForUser(user, Utils.getMomentToday())}
+                                onChangeDate={this.handleChangeDate}
+                            />
+                            <div className="clearfix" />
+                        </div>
+
+                        <fieldset className={`${styles.monthHeader} hide`}>
+                            <dl>
+                                <dt className="col-sm-3 col-md-1 col-lg-1">Datum</dt>
+                                <dt className="hidden-sm col-md-2 tt-col-lg-1 col-lg-1">Wochentag</dt>
+                                <dt className="col-sm-1 col-lg-1">Arbeitszeit</dt>
+                                <dt className="col-sm-1 tt-col-lg-1">Pause</dt>
+                                <dt className="col-sm-1 col-lg-1">Differenz</dt>
+                                <dt className="col-sm-4 col-lg-6">Kommentar</dt>
+                            </dl>
+                        </fieldset>
+
                     </div>
-
-                    <fieldset className={`${styles.monthHeader} hide`}>
-                        <dl>
-                            <dt className="col-sm-3 col-md-1 col-lg-1">Datum</dt>
-                            <dt className="hidden-sm col-md-2 tt-col-lg-1 col-lg-1">Wochentag</dt>
-                            <dt className="col-sm-1 col-lg-1">Arbeitszeit</dt>
-                            <dt className="col-sm-1 tt-col-lg-1">Pause</dt>
-                            <dt className="col-sm-1 col-lg-1">Differenz</dt>
-                            <dt className="col-sm-4 col-lg-6">Kommentar</dt>
-                        </dl>
-                    </fieldset>
-
                 </div>
-
+                <Footer />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { startServices } from './services';
 if (module.hot) {
     module.hot.accept('./App', () => {
         const NextApp = require('./App').default; // eslint-disable-line global-require
-        ReactDOM.render(<NextApp />, document.getElementById('root'));
+        ReactDOM.render(<NextApp />, document.getElementById('app'));
     });
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
 startServices();
