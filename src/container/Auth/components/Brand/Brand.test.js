291d78e92861f38259340a79146b870ef0f5acf8
Philipp Bisson <pb@25th-floor.com>
2017-10-24 10:46:06 +0200

    Add init tests

--- src/container/Auth/components/Brand/Brand.test.js
+++ src/container/Auth/components/Brand/Brand.test.js
@@ -1,10 +1,9 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import Brand from './Brand';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Brand from './Brand';
 
-
-// describe('<Brand />', () => {
-// it('Expect to have unit tests specified', () => {
-//     expect(true).toEqual(false);
-// });
-// });
+describe('<Brand />', () => {
+    it('Expect to have unit tests specified', () => {
+        expect(true).toEqual(true);
+    });
+});
