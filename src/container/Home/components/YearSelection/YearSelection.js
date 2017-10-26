98df28d8ce23d3e7f7bf6ba96fe683c85b3542c5
Thomas Subera <ts@25th-floor.com>
2017-10-26 13:57:07 +0200

    refactor YearSelection and styles

--- src/container/Home/components/YearSelection/YearSelection.js
+++ src/container/Home/components/YearSelection/YearSelection.js
@@ -4,63 +4,79 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import classSet from 'class-set';
 
+import type Moment from 'moment';
+
 import { Utils } from '@data';
 
 import styles from './YearSelection.module.css';
 
+type YearProps = {
+    activeMonth: Moment,
+    year: Moment,
+    today: Moment,
+};
+const Year = ({ year, activeMonth, today }: YearProps) => {
+    const className = classSet(
+        'col-xs-1',
+        styles.year,
+        year.isSame(activeMonth, 'year') ? styles.active : null,
+        year.isSame(today, 'year') ? styles.today : null,
+        year.isAfter(today, 'year') ? styles.future : null,
+    );
+    const fmtYear = activeMonth.clone().year(year.format('YYYY')).format('YYYY-MM');
+    return (
+        <li className={className} key={fmtYear}>
+            <NavLink
+                to={`/month/${fmtYear}`}
+                activeClassName={`${styles.active}`}
+            >
+                <span className={styles.number}>{year.format('YY')}</span>
+                <span className={styles.short}>{year.format('YYYY')}</span>
+            </NavLink>
+        </li>
+    );
+};
+
+type TodayProps = {
+    activeMonth: Moment,
+};
+const Today = ({ activeMonth }: TodayProps) => {
+    if (activeMonth.isSame(moment(), 'year')) {
+        return null;
+    }
+    const className = classSet(
+        'col-xs-1',
+        styles.todayButton,
+    );
+    const fmtToday = moment().startOf('month').format('YYYY-MM');
+    return (
+        <li className={className}>
+            <NavLink to={`/month/${fmtToday}`} activeClassName="active"> Today </NavLink>
+        </li>
+    );
+};
+
 export type YearSelectionProps = {
+    activeMonth: Moment,
+    years: Array<Moment>,
 };
 
 /**
  * YearSelection
  */
-
 export class YearSelection extends Component {
     props: YearSelectionProps;
     today = Utils.getMomentToday();
 
-    renderYearItem = (year, index) => {
-        const format = 'YYYY';
-        const className = classSet('col-xs-1',
-            year.isSame(this.props.activeMonth, 'year') ? styles.active : null,
-            year.isSame(this.today, 'year') ? styles.today : null,
-            year.isAfter(this.today, 'year') ? styles.future : null,
-        );
-        const fmtYear = this.props.activeMonth.clone().year(year.format('YYYY')).format('YYYY-MM');
-        return (
-            <li className={className} key={index}>
-                <NavLink
-                    to={`/month/${fmtYear}`}
-                    activeClassName={`${styles.active}`}
-                >
-                    <span className={styles.number}>{year.format('YY')}</span>
-                    <span className={styles.short}>{year.format('YYYY')}</span>
-                </NavLink>
-            </li>
-        );
-    }
-
-    renderTodayButton() {
-        if (this.props.activeMonth.isSame(moment(), 'year')) {
-            return '';
-        }
-        const fmtToday = moment().startOf('month').format('YYYY-MM');
-        return (
-            <li className={`${styles.today} col-sm-1 ${styles['tt-button-today']}`}>
-                <NavLink to={`/month/${fmtToday}`} activeClassName="active" > Today </NavLink>
-            </li>
-        );
-    }
-
     render() {
-        const years = this.props.years;
+        const { years, activeMonth } = this.props;
         return (
             <div className={styles.yearSelection}>
                 <h2>Year</h2>
                 <ul>
-                    {years.map(this.renderYearItem)}
+                    {years.map(y => (<Year year={y} today={this.today} activeMonth={activeMonth} />))}
 
-                    {this.renderTodayButton()}
+                    <Today activeMonth={activeMonth} />
                 </ul>
             </div>
         );
