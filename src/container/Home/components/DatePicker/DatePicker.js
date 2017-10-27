03efdcdabc231c05c8c8f618cdaae16148cf90d3
Thomas Subera <ts@25th-floor.com>
2017-10-27 12:44:22 +0200

    add flow typing to DatePicker component

--- src/container/Home/components/DatePicker/DatePicker.js
+++ src/container/Home/components/DatePicker/DatePicker.js
@@ -1,12 +1,18 @@
 // @flow
 
 import React, { Component } from 'react';
+
+import type Moment from 'moment';
+
 import { YearSelection } from '../YearSelection';
 import { MonthSelection } from '../MonthSelection';
 
 import styles from './DatePicker.module.css';
 
 export type DatePickerProps = {
+    activeMonth: Moment,
+    years: Array<Moment>,
+    months: Array<Moment>,
 };
 
 /**
