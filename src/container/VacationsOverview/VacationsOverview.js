01e37f325a826a7ee4ce93f63dcc699bc76a9590
Thomas Subera <ts@25th-floor.com>
2017-11-02 21:06:10 +0100

    style table

--- src/container/VacationsOverview/VacationsOverview.js
+++ src/container/VacationsOverview/VacationsOverview.js
@@ -3,8 +3,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import moment from 'moment';
 
-import { Resources } from '@data';
+import { Resources, Utils } from '@data';
 import { Footer } from '@components';
 
 import type { ApiVacationsType, ApiVacationType } from '@data/Resources/ResourcesTypes';
@@ -26,11 +27,31 @@ type VacationProps = {
     vacation: ApiVacationType,
 };
 
-const Vacation = ({ vacation }: VacationProps) => (
-    <div>
-        {vacation.usr_firstname}
-    </div>
-);
+const Vacation = ({ vacation }: VacationProps) => {
+    const date = moment(vacation.day_date);
+    const fullDate = date.format('DD.MM.YYYY');
+    const shortDate = date.format('DD.MM');
+
+    const targetTime = Utils.formatDurationHoursToLocale(moment.duration(vacation.day_target_time));
+    return (
+        <fieldset className="">
+            <dl>
+                <dt className="visible-xs">Datum</dt>
+                <dd className="hidden-sm col-md-1">{fullDate}</dd>
+                <dd className="col-sm-1 visible-sm">{shortDate}</dd>
+
+                <dt className="visible-xs hidden-sm">Sollzeit</dt>
+                <dd className="col-sm-1 col-lg-1 hidden-sm">{targetTime}</dd>
+
+                <dt className="visible-xs">User</dt>
+                <dd className="col-sm-4 col-md-3 col-lg-2">{vacation.usr_firstname} {vacation.usr_lastname}</dd>
+
+                <dt className="visible-xs">Kommentar</dt>
+                <dd className="col-sm-6 col-md-7 col-lg-8">{vacation.per_comment}</dd>
+            </dl>
+        </fieldset>
+    );
+};
 
 export type VacationOverviewProps = {
     isAuthenticated: boolean,
@@ -68,7 +89,14 @@ export class VacationsOverviewContainer extends Component<VacationOverviewProps,
         return (
             <div className={styles['site-container']}>
                 <div className="container-fluid">
-                    Vacancy
+                    <fieldset className="hidden-xs">
+                        <dl>
+                            <dt className="col-sm-2 col-md-1">Datum</dt>
+                            <dt className="hidden-sm col-sm-1">Sollzeit</dt>
+                            <dt className="col-sm-4 col-md-3 col-lg-2">User</dt>
+                            <dt className="col-sm-6 col-md-7 col-lg-8">Kommentar</dt>
+                        </dl>
+                    </fieldset>
                 </div>
                 {this.state.vacations.vacations.map((vacation, index) => <Vacation vacation={vacation} key={index} />)}
                 <Footer />
