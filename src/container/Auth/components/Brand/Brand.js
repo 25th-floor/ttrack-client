d68f63ff86009c379b8b0278bc537bde2a1ee7c3
Thomas Subera <ts@25th-floor.com>
2018-03-08 08:38:38 +0100

    refactor logo into it's own component
    also change svg path to a simpler version

--- src/container/Auth/components/Brand/Brand.js
+++ src/container/Auth/components/Brand/Brand.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Motto } from '@components';
+import { Motto, TTLogo } from '@components';
 
 import styles from './Brand.module.css';
 
@@ -8,23 +8,7 @@ import styles from './Brand.module.css';
  */
 export const Brand = () => (
     <div className={styles.loginBrand}>
-        <svg viewBox="0 0 100 100">
-            <path
-                d="M 100.00,0.00
-                       C 100.00,0.00 100.00,20.00 100.00,20.00
-                         100.00,20.00 80.00,20.00 80.00,20.00
-                         80.00,20.00 80.00,100.00 80.00,100.00
-                         80.00,100.00 60.00,100.00 60.00,100.00
-                         60.00,100.00 60.00,20.00 60.00,20.00
-                         60.00,20.00 40.00,20.00 40.00,20.00
-                         40.00,20.00 40.00,100.00 40.00,100.00
-                         40.00,100.00 20.00,100.00 20.00,100.00
-                         20.00,100.00 20.00,20.00 20.00,20.00
-                         20.00,20.00 0.00,20.00 0.00,20.00
-                         0.00,20.00 0.00,0.00 0.00,0.00
-                         0.00,0.00 100.00,0.00 100.00,0.00 Z"
-            />
-        </svg>
+        <TTLogo />
         <span className={styles.title}>
             <strong>Time</strong> Tracking
         </span>
